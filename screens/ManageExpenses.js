65af31a2af728dcd2a57361e818f062cef288ef1
Karlis <karlisstekels.dev@gmail.com>
2022-11-02 11:39:11 +0200

    Create Screens and implement Stack and BottomTab navigation

---
+++ screens/ManageExpenses.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from "react-native";
+
+const ManageExpense = () => {
+  return <Text>ManageExpense Screen!</Text>;
+};
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({});
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from "react-native";
+
+const RecentExpenses = () => {
+  return <Text>RecentExpenses Screen!</Text>;
+};
+
+export default RecentExpenses;
+
+const styles = StyleSheet.create({});
